6ff189bcf8c4c8e6cb590e69e9b419711af6a2e4
Cameron Steel <csteel890@gmail.com>
2026-08-06 21:23:41 +0100

    Docker image build

--- hello.py
+++ hello.py
@@ -8,11 +8,9 @@ print('Hello world!')
 print('Learning Git!')
 print('This is my third change...')
 
-name = input("What is your name? ")
+# name = input("What is your name? ")
 
 current_time = datetime.now()
-
-print(f"Hello {name}!")
 
 
 print('This is another Sunday evening print statement!')
